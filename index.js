cb21afaa134fb63b2dec4a9db1f6505916b37464
ionflare <ccionflare@gmail.com>
2018-06-14 05:20:07 +0000

    test

--- index.js
+++ index.js
@@ -401,15 +401,15 @@ function replyText(client,replyToken, returnStr,postBackStr) {
 function CalDistanceKm(lat1,lon1,lat2,lon2) {
 
    return new Promise( ( resolve, reject ) => {
-        var R = 6371; // Radius of the earth in km
-         var dLat = deg2rad(lat2-lat1);  // deg2rad below
-        var dLon = deg2rad(lon2-lon1); 
-         var a = 
+        let R = 6371; // Radius of the earth in km
+         let dLat = deg2rad(lat2-lat1);  // deg2rad below
+        let dLon = deg2rad(lon2-lon1); 
+         let a = 
             Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
             Math.sin(dLon/2) * Math.sin(dLon/2); 
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        var d = R * c; 
+        let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        let d = R * c; 
          resolve(d);
   } );
 }
